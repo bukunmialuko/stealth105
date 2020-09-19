58d5e3be2630378f8660dbab93b96de2f2c433f2
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-20 00:48:42 +0100

    One to many unidirectional delete example, using course and reviews

---
+++ src/com/example/stealth105/demo/onetomanyuni/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,67 @@
+package com.example.stealth105.demo.onetomanyuni;
+
+
+import com.example.stealth105.demo.enitity.Course;
+import com.example.stealth105.demo.enitity.Instructor;
+import com.example.stealth105.demo.enitity.InstructorDetail;
+import com.example.stealth105.demo.enitity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class DeleteCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the course
+            int theId = 10;
+            Course tempCourse = session.get(Course.class, theId);
+
+            // print the course
+            System.out.println("Deleting the course ... ");
+            System.out.println(tempCourse);
+
+            // print the course reviews
+            System.out.println(tempCourse.getReviews());
+
+            // delete the course
+            session.delete(tempCourse);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+        finally {
+
+            // add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+
+}
+
+
+
+
+
