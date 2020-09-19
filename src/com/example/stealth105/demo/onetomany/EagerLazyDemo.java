e3dfe0688b87aeef3a0517f4e0e1ed85f428b274
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-19 21:01:48 +0100

    Example of lazy load and how it will throw if you dont prefetch in memory before closing the session

---
+++ src/com/example/stealth105/demo/onetomany/EagerLazyDemo.java
@@ -0,0 +1,69 @@
+package com.example.stealth105.demo.onetomany;
+
+
+import com.example.stealth105.demo.enitity.Course;
+import com.example.stealth105.demo.enitity.Instructor;
+import com.example.stealth105.demo.enitity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class EagerLazyDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor from db
+            int theId = 1;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
+
+            System.out.println("\nluv2code: Instructor: " + tempInstructor +"\n");
+
+           // System.out.println("\nluv2code: Courses: " + tempInstructor.getCourses()+"\n");
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            // close the session
+            session.close();
+
+            System.out.println("\nluv2code: The session is now closed!\n");
+
+            // option 1: call getter method while session is open
+
+            // get courses for the instructor
+            System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+
+            System.out.println("luv2code: Done!");
+        }
+        finally {
+
+            // add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+
+}
+
+
+
+
+
