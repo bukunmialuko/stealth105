036b59518e0c5a7d9a77fd024d7cb2f1602e6dc9
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-19 12:31:20 +0100

    Removed some files as the tutor did. I was having some issues with unable to resolve table and colums

--- src/com/example/stealth105/jdbc/TestJdbc.java
+++ src/com/example/stealth105/jdbc/TestJdbc.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class TestJdbc {
     public static void main(String[] args) {
 
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
-        String user = "hbstudent";
-        String pass = "hbstudent";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String pass = "password123";
 
         try {
             System.out.println("Connecting to database: " + jdbcUrl);
